fed703ced328b4b6dc3aa38c8b9c41de722d0081
Alexander Belousov <pegas206@mail.ru>
2022-11-27 20:21:13 +0700

    pick

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
 import ru.netology.magicbox.MagicBox;
 
 public class Main {
-        public static void main(String[] args) {
-            System.out.println("==============================================================================");
-            MagicBox<Number> magicBox = new MagicBox<>("Волшебный ящик", 4);
-            magicBox.add(10);
-            magicBox.add(200);
-            magicBox.add(1000);
-            magicBox.add(555);
+    public static void main(String[] args) {
+        System.out.println("==============================================================================");
+        MagicBox<Number> magicBox = new MagicBox<>("Волшебный ящик", 4);
+        magicBox.add(10);
+        magicBox.add(200);
+        magicBox.add(1000);
+        magicBox.add(555);
+
+        magicBox.pick();
+
+        System.out.println("==============================================================================");
 
 
-            System.out.print("Случайное значение: "); magicBox.Random();
-
-            System.out.println("==============================================================================");
-
-
-
-}
+    }
 }
--- src/ru/netology/magicbox/MagicBox.java
+++ src/ru/netology/magicbox/MagicBox.java
@@ -4,32 +4,43 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class MagicBox<T> {
-        protected String name;
-        protected T[] items;
-        public MagicBox(String name, int maxAccount) {
-            this.name = name;
-            items = (T[]) new Object[maxAccount];
-        }
+    protected String name;
+    protected T[] items;
 
-       public boolean add(T item) {
-            for (int i = 0; i < items.length; i++) {
-                if (items[i] == null) {
-                    System.out.println("Добавлено ячейка " + i + " с значением " + item);
-                    items[i] = item;
-                    return true;
-                }
+    public MagicBox(String name, int maxAccount) {
+        this.name = name;
+        items = (T[]) new Object[maxAccount];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                System.out.println("Добавлено ячейка " + i + " с значением " + item);
+                items[i] = item;
+                return true;
             }
-            System.out.println("Место в волшебном ящике закончилось! :(");
-            System.out.println();
-            return false;
         }
+        System.out.println("Место в волшебном ящике закончилось! :(");
+        System.out.println();
+        return false;
+    }
 
-        public T Random() {
-            Random random = new Random();
-            int randomInt = random.nextInt(items.length);
-            System.out.println(items[randomInt]);
-            return null;
+    public T Random() {
+        Random random = new Random();
+        int randomInt = random.nextInt(items.length);
+        System.out.println(items[randomInt]);
+        return null;
+    }
+
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Есть свободное место");
+            }
         }
-//
-//    }
+        System.out.print("Случайное значение: ");
+        Random();
+
+        return null;
+    }
 }

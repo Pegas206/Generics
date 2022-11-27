d0f145bdc9c3dda4d5539379244e9fa2caf3915f
Alexander Belousov <pegas206@mail.ru>
2022-11-27 19:28:09 +0700

    <T> = String

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,20 @@ import ru.netology.magicbox.MagicBox;
 
 public class Main {
         public static void main(String[] args) {
+            System.out.println("==============================================================================");
+            MagicBox<String> magicBox = new MagicBox<>("Волшебный ящик", 5);
+            magicBox.add("Малина");
+            magicBox.add("Монета");
+            magicBox.add("Шар");
+            magicBox.add("Звезда");
+            magicBox.add("Конфета");
+            magicBox.add("Кролик");
 
-            MagicBox client = new MagicBox("Yandex box", 3);
-//            client.add();
-//            client.add(new CreditAccount(100000));
-
+            System.out.print("Случайное значение: "); magicBox.Random();
 
             System.out.println("==============================================================================");
+
+
+
 }
 }
--- src/ru/netology/magicbox/MagicBox.java
+++ src/ru/netology/magicbox/MagicBox.java
@@ -1,47 +1,35 @@
 package ru.netology.magicbox;
 
 import java.util.Arrays;
+import java.util.Random;
 
-public class MagicBox {
+public class MagicBox<T> {
         protected String name;
-        protected Arrays[] box;
+        protected T[] items;
         public MagicBox(String name, int maxAccount) {
             this.name = name;
-            box = new Arrays[maxAccount];
+            items = (T[]) new Object[maxAccount];
         }
 
-
-        public void add(Arrays cellBox) {
-            for (int i = 0; i < box.length; i++) {
-                if (box[i] == null) {
-                    box[i] = cellBox;
-                    System.out.println("Добавлен счет " + box);
-                    return;
+       public boolean add(T item) {
+            for (int i = 0; i < items.length; i++) {
+                if (items[i] == null) {
+                    System.out.println("Добавлено ячейка " + i + " с значением " + item);
+                    items[i] = item;
+                    return true;
                 }
             }
-            System.out.println("Мест в волшебном ящике закончилось! :(");
+            System.out.println("Место в волшебном ящике закончилось! :(");
             System.out.println();
+            return false;
         }
-//    public class Shop<P extends Product> {
-//        private List<P> items = new ArrayList<>();
-//        private Random random = new Random();
+
+        public T Random() {
+            Random random = new Random();
+            int randomInt = random.nextInt(items.length);
+            System.out.println(items[randomInt]);
+            return null;
+        }
 //
-//        public void add(P product) {
-//            items.add(product);
-//        }
-//
-//        public P getRandom() {
-//            return items.get(random.nextInt(items.size()));
-//        }
-//
-//        public P getCheapest() {
-//            int minIndex = 0;
-//            for (int i = 1; i < items.size(); i++) {
-//                if (items.get(minIndex).getPrice() > items.get(i).getPrice()) {
-//                    minIndex = i;
-//                }
-//            }
-//            return items.get(minIndex);
-//    }
 //    }
 }
